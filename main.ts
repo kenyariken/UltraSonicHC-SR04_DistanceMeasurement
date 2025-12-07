279d734e103736743ae6e5242b5a6d3818368362
Ken <161325958+kenyariken@users.noreply.github.com>
2025-12-07 20:51:22 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,10 @@
+let distance = 0
+basic.forever(function () {
+    distance = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )
+    basic.showNumber(distance)
+    basic.pause(2000)
+})
